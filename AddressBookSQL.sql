e091a882f46285b438992d0806e0fa2551b1915f
saurabhs <saurabhpshirsath@gmail.com>
2021-09-05 12:54:59 +0530

    [saurabh]Add:Table Created in DataBase

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -1,3 +1,17 @@
 Create Database AddressBookSystem
 
 Use AddressBookSystem
+
+Create Table AddressBook(
+ContactId int identity(1,1) Primary Key,
+FirstName varchar(20) not null,
+LastName varchar(20) not null,
+Address varchar(100) not null, 
+City varchar(50) not null,
+State varchar(200) not null,
+Zip bigint not null ,
+PhoneNumber bigint not null,
+Email varchar(50)not null
+)
+
+Select * From AddressBook
